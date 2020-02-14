4e6412988297f32aafd0414b38a7d56745296d7c
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-02-14 23:28:51 +0300

    first commit. All done

---
+++ README.md
@@ -0,0 +1,2 @@
+<div><img src="https://ci.appveyor.com/api/projects/status/bsao6suf5hm7nflk?svg=true"></img></div>
+# 7-1-matchers
---
+++ src/js/basic.js
@@ -0,0 +1,9 @@
+export default function getHealth({ name, health }) {
+  if (health > 50) return 'healthy';
+  if (health >= 15) return 'wounded';
+  return 'critical';
+}
+
+export function sortByHealth(heroes) {
+  return heroes.sort((a, b) => (a.health < b.health ? 1 : -1));
+}
